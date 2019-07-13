e4f7b124bfde97c04b7d991e81e498d47db9c4f2
pivey <peteriveyhansen@gmail.com>
2019-07-14 00:35:36 +0200

    added routing + connected to mongoDB database

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const port = process.env.port || 3001;
 const dataFromDB = require('./db.json');
 const Joi = require('@hapi/joi');
 const API_caller = require('./API_caller');
-const axios = require('axios');
+const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
+const Bear = require('./models/bear');
+const bodyParser = require('body-parser');
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -15,11 +18,11 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 /*
-
-//./node_modules/nodemon/bin/nodemon.js express.js
-
-
   when making an HTTP POST request The values are sent in the request body, 
   in the format that the content type specifies. Usually the content type is 
   application/x-www-form-urlencoded, so the request body uses the same format 
@@ -32,31 +35,65 @@ app.use(express.json());
   middleware employed you do not have parsed body param in your req.body.
 */
 
+/******************************** Connect to MongoDB **********************************/
+
+// // Connection URL
+// const url = 'mongodb://localhost:27017';
+
+// // Database Name
+// const dbName = 'db';
+
+// // Use connect method to connect to the server
+// MongoClient.connect(url, function(err, client) {
+//   assert.equal(null, err);
+//   console.log("Connected successfully to server");
+
+//   const db = client.db(dbName);
+
+//   client.close();
+// });
+
+// Connect to the db
+mongoose.connect("mongodb://localhost:27017/db", {
+  useNewUrlParser: true
+}, function (err, db) {
+  if (err) {
+    console.log("connection failed");
+  } else if (!err) {
+    console.log('connection established');
+  }
+});
+
+/******************************************************************/
+
 // Route handling: 
 
 //Creating Router() object
 
-// const router = express.Router();
+const router = express.Router();
 
 // ************************** Router middleware, mentioned it before defining routes.
 
-// router.use((req, res, next) => {
-//   console.log('/' + req.method);
-//   next();
-// });
+router.use((req, res, next) => {
+  console.log('/' + req.method);
+  const today = new Date();
+  const time = today.getHours() + ":" + today.getMinutes();
+  console.log('we have lift off: ', time);
+  next();
+});
 
-// router.use("/user/:id", function (req, res, next) {
-//   if (req.params.id == 0) {
-//     res.json({
-//       "message": "You must pass ID other than 0"
-//     });
-//   } else next();
-// });
+router.use("/user/:id", function (req, res, next) {
+  if (req.params.id == 0) {
+    res.json({
+      "message": "You must pass ID other than 0"
+    });
+  } else next();
+});
 
 // ************************** Provide all routes here, this is for Home page.
 
-//app.get('/', (req, res) => res.send('Hello world'));
-//app.get('/', (req,res) => res.set('content-Type', 'text/html'));
+app.get('/', (req, res) => res.send('Hello world'));
+app.get('/', (req, res) => res.set('content-Type', 'text/html'));
 
 
 // router.get('/', (req, res) => {
@@ -80,170 +117,244 @@ app.use(express.json());
 //   res.send(call);
 // });
 
-// app.use("/api", router);
+router.route('/bears')
+  .post(function (req, res) {
 
-// // handle 404 errors 
-// app.use(function(req, res, next) {
-//   return res.status(404).send({ message: 'Route'+req.url+' Not found.' });
-// });
+    var bear = new Bear();
+    bear.name = req.body.name;
 
-// // 500 - Any server error
-// app.use(function(err, req, res, next) {
-//   return res.status(500).send({ error: err });
-// });
+    bear.save(function (err) {
+      if (err)
+        res.send(err);
 
-// ********************************************************************************************************
-
-const errorObj = {
-  noID: 'The requested id does not exist, please try again'
-}
-
-app.get('/', (req, res) => res.send('Hello World'));
-
-app.get('/api/population', (req, res) => {
-  fs.readFile(db, 'utf8', (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      let obj = JSON.parse(data);
-      res.send(obj);
-    }
-  });
-});
-
-app.get('/api/population/:id', (req, res) => {
-
-  fs.readFile(db, 'utf8', (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      let obj = JSON.parse(data);
-      const foundPerson = obj.population.find((e) => e.id === parseInt(req.params.id));
-      !foundPerson || foundPerson === undefined ? res.status(404).send(errorObj.noID) : res.send(foundPerson);
-    }
-  });
-});
-
-function validateInput(newPerson) {
-  const schema = Joi.object().keys({
-    id: Joi.number(),
-    name: Joi.string().trim().min(2).max(30).required(),
-    name: Joi.string().trim().min(2).max(30).valid('nick').required().label('only mad decent guys are allowed'),
-    lastName: Joi.string().trim().min(2).max(30).required(),
-    email: Joi.string().trim().email().required(),
-    address: Joi.string().trim().regex(/^[\w\d\s\-\.\,]*$/i).max(30).required()
-  })
-  // return Joi.validate(newPerson, schema);
-  return Joi.validate(newPerson, schema, {
-    abortEarly: false
-  });
-}
-
-
-app.post('/api/population', (req, res) => {
-
-  const newPerson = {
-    id: dataFromDB.population.length + 1,
-    name: req.body.name,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    address: req.body.address,
-  }
-
-  const {
-    error
-  } = validateInput(newPerson);
-
-  if (error) {
-    const errObj = {};
-    for (let i = 0; i < error.details.length; i += 1) {
-      errObj[i] = error.details[i].message;
-    }
-    res.status(404).send(errObj);
-  } else {
-    fs.readFile(db, 'utf8', readFileCB = (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        obj = JSON.parse(data);
-        obj.population.push(newPerson); //add some data
-        json = JSON.stringify(obj); //convert it back to json
-
-        fs.writeFile(db, json, 'utf8', (err) => {
-          if (err) throw err;
-          console.log('The file has been saved!'); // write it to db
-        })
-      };
+      res.json({
+        message: 'Bear created!' + ' ' + bear
+      });
     });
 
-    res.send(newPerson);
-  }
+  })
 
-});
+  .get(function (req, res) {
+    Bear.find(function (err, bears) {
+      if (err) res.send(err);
+      res.json(bears);
+    });
+  });
 
-app.put('/api/population/:id', (req, res) => {
+router.route('/bears/:bear_id')
+  .get(function (req, res) {
+    Bear.findById(req.params.bear_id, function (err, bear) {
+      if (err)
+        res.send(err);
+      res.json(bear);
+    });
+  })
 
-  fs.readFile(db, 'utf8', (err, data) => {
-    if (!data) {
-      console.log(err);
-      console.log('Error with database');
-    } else {
-      let obj = JSON.parse(data);
-      let foundPerson = obj.population.find((e) => e.id === parseInt(req.params.id));
-      const {
-        error
-      } = validateInput(req.body);
+  .put(function (req, res) {
 
-      console.log(error);
+    Bear.findById(req.params.bear_id, function (err, bear) {
 
-      if (error) {
-        const errObj = {};
-        for (let i = 0; i < error.details.length; i += 1) {
-          errObj[i] = error.details[i].message;
-        }
-        res.status(404).send(errObj);
-      } else {
-        foundPerson.name = req.body.name;
-        foundPerson.lastName = req.body.lastName;
-        foundPerson.email = req.body.email;
-        foundPerson.address = req.body.address;
+      if (err)
+        res.send(err);
 
-        obj.population.splice(obj.population.indexOf(foundPerson), 1, foundPerson);
+      bear.name = req.body.name;
 
-        fs.writeFile(db, JSON.stringify(obj), 'utf8', (err) => {
-          if (err) throw err;
-          console.log('could not write to file');
-        })
+      bear.save(function (err) {
+        if (err)
+          res.send(err);
 
-        res.send(foundPerson);
-      }
-    }
+        res.json({
+          message: 'Bear updated!' + ' ' + bear
+        });
+      });
+
+    });
+  })
+
+  .delete(function (req, res) {
+    Bear.remove({
+      _id: req.params.bear_id
+    }, function (err, bear) {
+      if (err)
+        res.send(err);
+
+      res.json({
+        message: 'Successfully deleted'
+      });
+    });
+  });
+
+
+
+
+app.use("/api", router);
+
+// handle 404 errors 
+app.use(function (req, res, next) {
+  return res.status(404).send({
+    message: 'Route' + req.url + ' Not found.'
   });
 });
 
-app.delete('/api/population/:id', (req, res) => {
-
-  fs.readFile(db, 'utf8', (err, data) => {
-    if (!data) {
-      console.log(err);
-      console.log('Error with database');
-    } else {
-      let obj = JSON.parse(data);
-
-      let foundPerson = obj.population.find((e) => e.id === parseInt(req.params.id));
-      !foundPerson || foundPerson === undefined ? res.status(404).send(errorObj.noID) :
-
-
-        res.send(obj.population.splice(obj.population.indexOf(foundPerson), 1));
-
-      fs.writeFile(db, JSON.stringify(obj), 'utf8', (err) => {
-        if (err) {
-          throw err;
-        }
-        console.log('could not write to file');
-      })
-    }
+// 500 - Any server error
+app.use(function (err, req, res, next) {
+  return res.status(500).send({
+    error: err
   });
 });
+
+// ************************************************ API Endpoints ********************************************************
+
+// const errorObj = {
+//   noID: 'The requested id does not exist, please try again'
+// }
+
+// app.get('/', (req, res) => res.send('Hello World'));
+
+// app.get('/api/population', (req, res) => {
+//   fs.readFile(db, 'utf8', (err, data) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       let obj = JSON.parse(data);
+//       res.send(obj);
+//     }
+//   });
+// });
+
+// app.get('/api/population/:id', (req, res) => {
+
+//   fs.readFile(db, 'utf8', (err, data) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       let obj = JSON.parse(data);
+//       const foundPerson = obj.population.find((e) => e.id === parseInt(req.params.id));
+//       !foundPerson || foundPerson === undefined ? res.status(404).send(errorObj.noID) : res.send(foundPerson);
+//     }
+//   });
+// });
+
+// function validateInput(newPerson) {
+//   const schema = Joi.object().keys({
+//     id: Joi.number(),
+//     name: Joi.string().trim().min(2).max(30).required(),
+//     name: Joi.string().trim().min(2).max(30).valid('nick').required().label('only mad decent guys are allowed'),
+//     lastName: Joi.string().trim().min(2).max(30).required(),
+//     email: Joi.string().trim().email().required(),
+//     address: Joi.string().trim().regex(/^[\w\d\s\-\.\,]*$/i).max(30).required()
+//   })
+//   // return Joi.validate(newPerson, schema);
+//   return Joi.validate(newPerson, schema, {
+//     abortEarly: false
+//   });
+// }
+
+
+// app.post('/api/population', (req, res) => {
+
+//   const newPerson = {
+//     id: dataFromDB.population.length + 1,
+//     name: req.body.name,
+//     lastName: req.body.lastName,
+//     email: req.body.email,
+//     address: req.body.address,
+//   }
+
+//   const {
+//     error
+//   } = validateInput(newPerson);
+
+//   if (error) {
+//     const errObj = {};
+//     for (let i = 0; i < error.details.length; i += 1) {
+//       errObj[i] = error.details[i].message;
+//     }
+//     res.status(404).send(errObj);
+//   } else {
+//     fs.readFile(db, 'utf8', readFileCB = (err, data) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         obj = JSON.parse(data);
+//         obj.population.push(newPerson); //add some data
+//         json = JSON.stringify(obj); //convert it back to json
+
+//         fs.writeFile(db, json, 'utf8', (err) => {
+//           if (err) throw err;
+//           console.log('The file has been saved!'); // write it to db
+//         })
+//       };
+//     });
+
+//     res.send(newPerson);
+//   }
+
+// });
+
+// app.put('/api/population/:id', (req, res) => {
+
+//   fs.readFile(db, 'utf8', (err, data) => {
+//     if (!data) {
+//       console.log(err);
+//       console.log('Error with database');
+//     } else {
+//       let obj = JSON.parse(data);
+//       let foundPerson = obj.population.find((e) => e.id === parseInt(req.params.id));
+//       const {
+//         error
+//       } = validateInput(req.body);
+
+//       console.log(error);
+
+//       if (error) {
+//         const errObj = {};
+//         for (let i = 0; i < error.details.length; i += 1) {
+//           errObj[i] = error.details[i].message;
+//         }
+//         res.status(404).send(errObj);
+//       } else {
+//         foundPerson.name = req.body.name;
+//         foundPerson.lastName = req.body.lastName;
+//         foundPerson.email = req.body.email;
+//         foundPerson.address = req.body.address;
+
+//         obj.population.splice(obj.population.indexOf(foundPerson), 1, foundPerson);
+
+//         fs.writeFile(db, JSON.stringify(obj), 'utf8', (err) => {
+//           if (err) throw err;
+//           console.log('could not write to file');
+//         })
+
+//         res.send(foundPerson);
+//       }
+//     }
+//   });
+// });
+
+// app.delete('/api/population/:id', (req, res) => {
+
+//   fs.readFile(db, 'utf8', (err, data) => {
+//     if (!data) {
+//       console.log(err);
+//       console.log('Error with database');
+//     } else {
+//       let obj = JSON.parse(data);
+
+//       let foundPerson = obj.population.find((e) => e.id === parseInt(req.params.id));
+//       !foundPerson || foundPerson === undefined ? res.status(404).send(errorObj.noID) :
+
+
+//         res.send(obj.population.splice(obj.population.indexOf(foundPerson), 1));
+
+//       fs.writeFile(db, JSON.stringify(obj), 'utf8', (err) => {
+//         if (err) {
+//           throw err;
+//         }
+//         console.log('could not write to file');
+//       })
+//     }
+//   });
+// });
 
 app.listen(port, () => console.log(`**** server is running on port ${port} ****`));
